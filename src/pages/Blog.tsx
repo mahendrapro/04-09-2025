d68e4c8ce99af5f27e89e18216cc2c85c98ac85b
hemanth647 <hemanth@thebrainy.agency>
2025-09-27 18:23:17 +0530

    Update Blog.tsx

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -310,7 +310,7 @@ const Blog = () => {
           </p>
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
             <Link
-              to="https://calendly.com/mahendra-thebrainy/30min"
+              to="https://calendly.com/hemanth-thebrainy/30min"
               target="_blank"
               rel="noopener noreferrer"
               className="bg-red-500 text-white px-8 py-4 rounded-lg font-semibold text-lg hover:bg-white hover:text-red-500 hover:border hover:border-red-500 transition-all duration-200 shadow-xl"
